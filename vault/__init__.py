dcd6b321fd3c1a3ab209549bcec190c8ad3a859c
joeltejeda <karma@Ubuntu-1804-bionic-64-minimal>
2021-05-11 19:58:11 +0200

    URL and password validation

--- vault/__init__.py
+++ vault/__init__.py
@@ -1,12 +1,14 @@
 import os
-
+import requests
 from flask import Flask, render_template, request
 import subprocess
+from urllib.parse import urlsplit
+
 def create_app(test_config=None):
     # create and configure the app
 
     app = Flask(__name__, template_folder='/home/karma/www/vault-karmacomputing/vault/template/')
-
+    path = './vault' 
 
     @app.route('/')
     def index():
@@ -15,16 +17,35 @@ def create_app(test_config=None):
 
     @app.route('/unlock', methods=["POST"])
     def unlock():
-          
-        subprocess.run("./unlock_lock.sh", shell=True)
         
-        return render_template('unlock.html')
-
+        if request.method == "POST":
+            r = requests.post(validate(request.form['websiteURL']), data = {'log':request.form['username'], 'pwd':request.form['password']})
+            #validating URL 
+            if "wp-login.php" in request.form['websiteURL'] and r.status_code == 200:
+                #validating Login
+                request_comparison = "login_error" in r.content.decode('utf-8')
+                if request_comparison == False: #True if password is wrong
+                    print (validate(request.form['websiteURL']))
+                    print ("Correct Password")
+                    # subprocess.run(f"{path}/unlock_lock.sh", shell=True)
+                    return render_template('unlock.html')
+                return("Wrong Password or Username")
+                print("Wrong Password")
+            print("invalid URL") 
+            return ("Please enter a valid wordpress URL")
+        return render_template('index.html')
+    
     @app.route('/lock', methods=["POST"])
     def lock():
+            print ("your site is locked")
+           #subprocess.run(f"{path}/lock.sh", shell=True)
+            return render_template('lock.html')
 
-        subprocess.run("./lock.sh", shell=True)
-
-        return render_template('lock.html')
+    def validate(webaddress: str):
+        if "://" in webaddress:
+            return webaddress
+        else: 
+            webaddress = "http://" + webaddress
+            return webaddress
 
     return app
--- vault/lock.sh
+++ vault/lock.sh
@@ -5,6 +5,13 @@ set -x
 DIRECTORY=/home/r2global/www/r2-commerce.com
 UPLOADS=/home/r2global/www/r2-commerce.com/wp-content/uploads
 
-#lock the Directory and Unlock the UPLOADS folder
-sudo chattr -R +i $DIRECTORY && chattr -R -i $UPLOADS && echo "Time as passed, all files are locked")&
+
+sudo chattr -R +i $DIRECTORY && sudo chattr -R -i $UPLOADS 
+
+if [[ $? -ne 0 ]]
+then
+   echo "Directory already locked"
+else
+   echo "All Files are locked"
+fi
 
