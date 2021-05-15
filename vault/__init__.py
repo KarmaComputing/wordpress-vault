d1f82b0c9acb94bfe6989b939707098ca358cba3
Joel <joel@karmacomputing.co.uk>
2021-05-14 23:53:53 -0400

    adding an env variable to compare sites

--- vault/__init__.py
+++ vault/__init__.py
@@ -1,10 +1,12 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import subprocess
 from dotenv import load_dotenv
 import os
 from urllib.parse import urlsplit
 import urllib
+import json
+import ast
 
 load_dotenv(verbose=True)  # get environment variables from .env
 
@@ -16,13 +18,18 @@ def create_app(test_config=None):
 
     @app.route("/")
     def index():
+        import pdb
+
+        pdb.set_trace()
+        return type(ast.literal_eval(app.config["ALLOWED_SITES"]))
         return render_template("index.html")
 
     @app.route("/unlock", methods=["POST"])
     def unlock():
-
         if request.method == "POST":
             domain = validate(request.form["websiteURL"]).geturl()
+            if domain not in app.config["ALLOWED_SITES"]:
+                return redirect("https://subscriptionwebsitebuilder.co.uk", code=301)
             if wordpress_login_success(domain) is False:
                 return "Wrong Password or Username"
             subprocess.run(app.config["PATH_TO_UNLOCK_SCRIPT"], shell=True)
@@ -31,7 +38,10 @@ def create_app(test_config=None):
 
     @app.route("/lock", methods=["POST"])
     def lock():
-        print("your site is locked")
+        if request.method == "POST":
+            domain = validate(request.form["websiteURL"]).geturl()
+            if domain not in app.config["ALLOWED_SITES"]:
+                return redirect("https://subscriptionwebsitebuilder.co.uk", code=301)
         subprocess.run(app.config["PATH_TO_LOCK_SCRIPT"], shell=True)
         return render_template("lock.html")
 
@@ -65,3 +75,11 @@ def validate(webaddress: str) -> urllib.parse.SplitResult:
     webaddress = webaddress if "://" in webaddress else "https://" + webaddress
     url = urlsplit(webaddress)
     return url
+
+
+def documentRoot(domain, dictionary):
+
+    for domain in dictionary:
+        print(domain)
+        print(dictionary[domain])
+        return dictionary[domain]
