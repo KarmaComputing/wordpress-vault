28c64fc04907df62e47e054e26d16f0e80d18d1b
Joel <joel@karmacomputing.co.uk>
2021-06-01 13:33:52 -0400

    deleting redundancy while activating env

---
+++ append-to-bash.py
@@ -0,0 +1,19 @@
+import json
+import os
+import subprocess
+
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+ALLOWED_SITES = os.getenv("ALLOWED_SITES")
+ALLOWED_SITES = json.loads(ALLOWED_SITES)  # converted to dict
+
+
+for site in ALLOWED_SITES.keys():
+
+    command = (
+        f"./update-WP.sh {ALLOWED_SITES[site]['path']} {ALLOWED_SITES[site]['user']}"
+    )
+    print(command)
+    subprocess.run(command, shell=True)  # noqa
--- run-directory.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -x
-set -e
-
-. venv/bin/activate && python3 append-to-bash.py && deactivate
-
